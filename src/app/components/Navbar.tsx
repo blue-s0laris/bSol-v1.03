325039c27c4df2a2d155913fbf84cd37404b3e28
blue-s0laris <pasqualetrinchella@gmail.com>
2025-02-22 12:47:00 +0100

    UI updates and Auth setup

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,25 +6,29 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-4 bg-[#ffffff] dark:bg-[#1a1613]  text-[#000000] dark:text-[#ffffff]">
-      <Link href="/" className="flex items-center gap-2">
-        <Image
-          src="/foxwhite.png"
-          alt="Logo"
-          width={29}
-          height={29}
-          className="h-7 w-7 hidden dark:block"
-        />
-        <Image
-          src="/foxblack.png"
-          alt="Logo"
-          width={29}
-          height={29}
-          className="h-7 w-7 dark:hidden"
-        />
+    <nav className="flex items-center justify-end p-4 bg-[#ffffff] dark:bg-[#1a1613]  text-[#000000] dark:text-[#ffffff]">
+      <div className="flex items-center">
+        <ThemeSwitch />
+      </div>
+      <Link href="/" className="flex items-center gap-2 ml-4">
         bSol v1.03
       </Link>
-      <ThemeSwitch />
+      <Image
+        src="/foxwhite.png"
+        alt="Logo"
+        quality={100}
+        width={29}
+        height={29}
+        className="h-6 w-6 hidden dark:block ml-4"
+      />
+      <Image
+        src="/foxblack.png"
+        alt="Logo"
+        quality={100}
+        width={29}
+        height={29}
+        className="h-6 w-6 dark:hidden ml-4"
+      />
     </nav>
   );
 }
