1be51664e5dd7d6a6cb0ee46f8372d0f4bada669
blue-s0laris <pasqualetrinchella@gmail.com>
2025-02-22 13:01:28 +0100

    auth building

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Credits";
 import { Providers } from "./providers";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const ubuntuSans = Ubuntu({
   variable: "--font-ubuntu-sans",
@@ -23,18 +31,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${ubuntuSans.variable} ${ubuntuSans.variable} antialiased`}
-      >
-        <Providers>
-          <Navbar />
-          <main className="min-h-screen bg-white dark:bg-[#1a1613] text-[#000000] dark:text-[#ffffff]">
-            {children}
-          </main>
-          <Footer />
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${ubuntuSans.variable} ${ubuntuSans.variable} antialiased`}
+        >
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <Providers>
+            <Navbar />
+            <main className="min-h-screen bg-white dark:bg-[#1a1613] text-[#000000] dark:text-[#ffffff]">
+              {children}
+            </main>
+            <Footer />
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
