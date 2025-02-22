0754ece4fc473b3c8116cf71d61890d964def6f2
blue-s0laris <pasqualetrinchella@gmail.com>
2025-02-22 02:30:26 +0100

    minor UI changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,16 +1,27 @@
 "use client";
 
 import Link from "next/link";
-import ThemeToggle from "./ThemeSwitch";
 import ThemeSwitch from "./ThemeSwitch";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-4 bg-gray-100 dark:bg-gray-900">
-      <Link
-        href="/"
-        className="text-xl font-bold text-gray-900 dark:text-white"
-      >
+    <nav className="flex items-center justify-between p-4 bg-[#ffffff] dark:bg-[#1a1613]  text-[#000000] dark:text-[#ffffff]">
+      <Link href="/" className="flex items-center gap-2">
+        <Image
+          src="/foxwhite.png"
+          alt="Logo"
+          width={29}
+          height={29}
+          className="h-7 w-7 hidden dark:block"
+        />
+        <Image
+          src="/foxblack.png"
+          alt="Logo"
+          width={29}
+          height={29}
+          className="h-7 w-7 dark:hidden"
+        />
         bSol v1.03
       </Link>
       <ThemeSwitch />
