6f2afa6dcb09e2d923928c8172184f43eee98968
blue-s0laris <pasqualetrinchella@gmail.com>
2025-02-24 10:15:47 +0100

    firebase config

--- src/app/components/Credits.tsx
+++ src/app/components/Credits.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-center p-4 bg-gray-100 dark:bg-gray-900 text-gray-600 dark:text-gray-300">
+    <footer className="text-center p-4 bg-[#ffffff] dark:bg-[#1a1613]  text-[#000000] dark:text-[#ffffff]">
       © {new Date().getFullYear()} bSol™ . All rights reserved.
     </footer>
   );
