d0538b7849785c4f515db35958835db953faaf7b
blue-s0laris <pasqualetrinchella@gmail.com>
2025-02-24 11:35:03 +0100

    typescript for config

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -1,18 +1,21 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { FirebaseApp, initializeApp, getApps, getApp } from "firebase/app";
+import { Auth, getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env
+    .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
 };
 
 // Initialize Firebase
 // for server side rendering
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+const app: FirebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
+const auth: Auth = getAuth(app);
 export { app, auth };
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,12 +1,33 @@
 "use client";
 
 import { useState } from "react";
+import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
+import {auth} from '@app/firebase/config'
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+};
 
 const handleSignUp = () => {
-  console.log('user signed up:' {email, password});
-}
+  console.log("user signed up:", { email, password });
+};
+
+return (
+  <div className="min-h-screen flex items-center justify-center bg-[#9b9b9b] dark:bg-[#1a1613]">
+    <div className="bg-[#9b9b9b] dark:bg-[#1a1613] p-10 rounded-lg shadow-xl w-96">
+      <h1 className="text-white text-2xl mb-5">sign up</h1>
+      <input type="email"
+      placeholder="email"
+      value={email}
+      onChange={(e) => setEmail.(e.target.value)}
+      className="w-full p-3 mb-4 bg-[#9b9b9b] dark:bg-[#1a1613] rounded outline-none text-white placeholder:bg-gray-500"/>
+      <input 
+      type="password"
+      placeholder="password"
+      value={password}
+      onChange={(e) => setPassword.(e.target.value)}
+      className="w-full p-3 mb-4 bg-[#9b9b9b] dark:bg-[#1a1613] rounded outline-none text-white placeholder:bg-gray-500"/>
+    </div>
+  </div>
+);
